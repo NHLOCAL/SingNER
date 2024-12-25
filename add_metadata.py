3014825a32ecd56932a4c08da1bec7da5bce05e8
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-12-25 05:52:06 +0200

    עדכון גרסה 2.0

--- add_metadata.py
+++ add_metadata.py
@@ -1,11 +1,11 @@
 import spacy
 
 # טען את המודל הקיים או צור חדש
-nlp = spacy.load("singer_ner_he")
+nlp = spacy.load("models/singer_ner_he")
 
 # עדכון המטא נתונים
 nlp.meta['name'] = 'singer_ner_he'
-nlp.meta['version'] = '1.0.0'
+nlp.meta['version'] = '2.0.0'
 nlp.meta['description'] = 'Model for recognizing singer names in Hebrew song titles'
 nlp.meta['author'] = 'nhlocal'
 nlp.meta['email'] = 'nh.local11@gmail.com'
@@ -13,5 +13,5 @@ nlp.meta['license'] = 'MIT'
 nlp.meta['tags'] = ['NER', 'Hebrew', 'Singer', 'Named Entity Recognition', 'Text Classification']
 
 # שמור את המודל מחדש
-nlp.to_disk("singer_ner_he")
+nlp.to_disk("models/singer_ner_he")
 print("the model with metadata saving to disk!")
